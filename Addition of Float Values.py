788e076e140b254343a2da829f7ddd8e4a8e7199
Anshuman Sinha <138311612+anshumansinha3301@users.noreply.github.com>
2024-02-14 09:27:48 +0530

    Add files via upload

---
+++ Addition of Float Values.py
@@ -0,0 +1,4 @@
+num1 = float(input("Enter the first floating-point number: "))
+num2 = float(input("Enter the second floating-point number: "))
+result = num1 + num2
+print("The sum of", num1, "and", num2, "is:", result)
