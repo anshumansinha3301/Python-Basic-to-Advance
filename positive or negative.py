d2b6edb549b8c99a12a13287d739b17e32f8dbf7
Anshuman Sinha <138311612+anshumansinha3301@users.noreply.github.com>
2024-02-16 09:29:45 +0530

    Add files via upload

---
+++ positive or negative.py
@@ -0,0 +1,9 @@
+number = float(input('Enter a number: '))
+
+# below are control flow statements (if, elif, else)
+if number > 0:
+    print('The number is positive.')
+elif number < 0:
+    print('The number is negative.')
+else:
+    print('The number is zero.')
