daca14daf11f5f460aed4d30e48c3fa4c8899e4e
Anshuman Sinha <138311612+anshumansinha3301@users.noreply.github.com>
2024-02-16 09:23:26 +0530

    Add files via upload

---
+++ Random Number.py
@@ -0,0 +1,6 @@
+'''random ka bhi module hai :), code like these gets used in game for RNG where any 
+treasure or mystery is hidden'''
+import random
+
+random_number = random.randint(0, 99999)
+print(f'Random number up to 5 digits: {random_number:05d}')
