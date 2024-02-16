160637593e49563340d71f2eb8d103277f7f73c7
Anshuman Sinha <138311612+anshumansinha3301@users.noreply.github.com>
2024-02-16 09:44:15 +0530

    Add files via upload

---
+++ largest among three.py
@@ -0,0 +1,7 @@
+num1 = float(input('Enter the first number: '))
+num2 = float(input('Enter the second number: '))
+num3 = float(input('Enter the third number: '))
+
+largest = max(num1, num2, num3)
+
+print(f'The largest number among {num1}, {num2}, and {num3} is: {largest}')
