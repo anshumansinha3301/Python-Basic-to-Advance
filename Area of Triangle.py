95732e7c491c39ead401b01916d3f73a2065e319
Anshuman Sinha <138311612+anshumansinha3301@users.noreply.github.com>
2024-02-16 09:15:31 +0530

    Add files via upload

---
+++ Area of Triangle.py
@@ -0,0 +1,16 @@
+#below line is function for calculating area
+def calculate_triangle_area(a, b, c):
+    #below code is for semi - perimeter
+    s = (a + b + c) / 2
+    
+    #heron's formula area calculation
+    area = (s * (s - a) * (s - b) * (s - c)) ** 0.5
+    
+    return area
+
+a = float(input('Enter the length of the first side: '))
+b = float(input('Enter the length of the second side: '))
+c = float(input('Enter the length of the third side: '))
+
+area = calculate_triangle_area(a, b, c)
+print(f'The area of the triangle is {area:.2f}')
