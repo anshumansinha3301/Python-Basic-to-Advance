4a895ddf4b76b69c738c8659f4f94556b4deec9e
Anshuman Sinha <138311612+anshumansinha3301@users.noreply.github.com>
2024-03-11 18:35:07 +0530

    square root.py

--- square root.py
+++ square root.py
@@ -2,7 +2,7 @@ import math
 
 num = float(input('Enter a number: '))
 
-#we use math sqrt function to find square root
+#we use math sqrt function to find square root in python
 num_sqrt = math.sqrt(num)
 
 print(f'The square root of {num} is {num_sqrt:.3f}')
