301dc5d4f3e843cad8d52a1aed80de4e9b14b897
Anshuman Sinha <138311612+anshumansinha3301@users.noreply.github.com>
2024-03-14 23:34:47 +0530

    celsius-fahrenheit.py

--- celsius-fahrenheit.py
+++ celsius-fahrenheit.py
@@ -1,6 +1,6 @@
 celsius = float(input('Enter temperature in Celsius: '))
 
-# 6th class ka formuala yaad hai ;)
+# 6th class ka formuala yaad hai ;) 
 fahrenheit = (celsius * 9/5) + 32
  
 print(f'Temperature in Fahrenheit: {fahrenheit:.2f}')
